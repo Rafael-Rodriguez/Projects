4f7c31e939530e6fb024fdda0d3116b1b49df315
Rafael <rrodrigu@gmail.com>
2017-05-14 16:34:31 -0700

    Added Calculate EventHandler callback

--- Murach's C# 2015/Exercise.01.InvoiceTotal/Exercise.01.InvoiceTotal/Perspectives/frmInvoiceTotal.cs
+++ Murach's C# 2015/Exercise.01.InvoiceTotal/Exercise.01.InvoiceTotal/Perspectives/frmInvoiceTotal.cs
@@ -16,5 +16,41 @@ namespace Exercise._01.InvoiceTotal
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            var subTotal = Convert.ToDecimal(txtBoxSubtotal.Text);
+            var discountPercent = 0m;
+
+            if(subTotal >= 500)
+            {
+                discountPercent = .2m;
+            }
+            else if (subTotal >= 250)
+            {
+                discountPercent = .15m;
+            }
+            else if(subTotal >= 100)
+            {
+                discountPercent = .1m;
+            }
+
+            var discountAmount = subTotal * discountPercent;
+            var invoiceTotal = subTotal - discountAmount;
+
+            txtBoxDiscountPercent.Text = discountPercent.ToString("p1");
+            txtBoxDiscountAmount.Text = discountAmount.ToString("c");
+            txtBoxTotal.Text = invoiceTotal.ToString("c");
+
+            txtBoxSubtotal.Focus();
+
+
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
