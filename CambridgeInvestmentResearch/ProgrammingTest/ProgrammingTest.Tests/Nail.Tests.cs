64d9efa1e3dbd7ca0255d68fa71a974d634b1f74
Rafael <rrodrigu@gmail.com>
2017-06-18 21:44:18 -0700

    [CambridgeInvestmentResearch] Checkin of submitted test.

---
+++ CambridgeInvestmentResearch/ProgrammingTest/ProgrammingTest.Tests/Nail.Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingTest.Tests
+{
+	/// <summary>
+	/// Summary description for Nail
+	/// </summary>
+	[TestClass]
+	public class NailTests
+	{
+		[TestMethod]
+		public void SolutionNails_3NailsOfLength3HammerIn2_ResultOf5()
+		{
+			//Arrange
+            var solutionNails = new SolutionNails();
+            var array = new int[] { 1, 1, 3, 3, 3, 4, 5, 5, 5, 5 };
+
+            //Act
+            var result = solutionNails.solution(array, 2);
+
+            //Assert
+            Assert.AreEqual(5, result);
+		}
+
+        [TestMethod]
+        public void SolutionNails_2NailsOfLength1HammerIn1_ResultOf3()
+        {
+            //Arrange
+            var solutionNails = new SolutionNails();
+            var array = new int[] { 1, 1, 3, 4, };
+
+            //Act
+            var result = solutionNails.solution(array, 1);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void SolutionNails_2NailsOfLength1HammerIn4Only3Nails_ResultOf0()
+        {
+            //Arrange
+            var solutionNails = new SolutionNails();
+            var array = new int[] { 1, 1, 3 };
+
+            //Act
+            var result = solutionNails.solution(array, 4);
+
+            //Assert
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ CambridgeInvestmentResearch/ProgrammingTest/ProgrammingTest.Tests/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgrammingTest.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void solution_a0ba_ReturnMinus1()
+        {
+            var solution = new Solution();
+
+            var result = solution.solution("a0ba");
+
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void solution_a0Ba_Return2()
+        {
+            //Arrange
+            var solution = new Solution();
+
+            //Act
+            var result = solution.solution("a0Ba");
+
+            //Assert
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void solution_a0BaBa2_Return4()
+        {
+            //Arrange
+            var solution = new Solution();
+
+            //Act
+            var result = solution.solution("a0BaBa2");
+
+            //Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void solution_Ba0Ba2_Return2()
+        {
+            //Arrange
+            var solution = new Solution();
+
+            //Act
+            var result = solution.solution("Ba0Ba2");
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void solution_100character_Return100()
+        {
+            //Arrange
+            var solution = new Solution();
+
+            //Act
+            var result = solution.solution("a0BABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABABA8b");
+
+            //Assert
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod]
+        public void solution_a0BABAB7ABaziga8b_Return7()
+        {
+            //Arrange
+            var solution = new Solution();
+
+            //Act
+            var result = solution.solution("a0BABAB7ABaziga8b");
+
+            //Assert
+            Assert.AreEqual(7, result);
+        }
+
+    }
+}
