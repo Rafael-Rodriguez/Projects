63132156c8becd12868b799f7377b5c2fcf150ae
rrodriguez <rrodriguez@TUCPD033>
2018-03-07 17:18:27 -0700

    Added MMABook data source.  Added Customers and Invoices table

--- Murach/Chapter19.CustomerInvoices/Chapter19.CustomerInvoices/frmCustomerInvoices.cs
+++ Murach/Chapter19.CustomerInvoices/Chapter19.CustomerInvoices/frmCustomerInvoices.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Chapter19.CustomerInvoices
 {
-    public partial class Form1 : Form
+    public partial class frmCustomerInvoices : Form
     {
-        public Form1()
+        public frmCustomerInvoices()
         {
             InitializeComponent();
         }
