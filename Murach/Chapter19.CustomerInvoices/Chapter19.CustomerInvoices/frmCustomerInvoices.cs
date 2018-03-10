77361846b7c84393569f4b8ba6c543daf1804daf
rrodrigu <rrodrigu@gmail.com>
2018-03-09 19:03:39 -0700

    Disabled Name, Address, City, State, and Zip text boxes. CustomerID text box now is the active control on form load

--- Murach/Chapter19.CustomerInvoices/Chapter19.CustomerInvoices/frmCustomerInvoices.cs
+++ Murach/Chapter19.CustomerInvoices/Chapter19.CustomerInvoices/frmCustomerInvoices.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Chapter19.CustomerInvoices
@@ -22,14 +15,11 @@ namespace Chapter19.CustomerInvoices
             this.Validate();
             this.customersBindingSource.EndEdit();
             this.tableAdapterManager.UpdateAll(this.mMABooksDataSet);
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'mMABooksDataSet.Customers' table. You can move, or remove it, as needed.
-            this.customersTableAdapter.Fill(this.mMABooksDataSet.Customers);
-
+            this.ActiveControl = customerIDToolStripTextBox.TextBox;
         }
 
         private void fillByCustomerIDToolStripButton_Click(object sender, EventArgs e)
