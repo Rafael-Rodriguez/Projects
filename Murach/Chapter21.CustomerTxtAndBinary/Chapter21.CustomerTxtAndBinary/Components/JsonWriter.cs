5bc61000b87bdf79b4a62485c4d2d906a051b121
rrodriguez <rrodriguez@TUCPD033>
2018-03-29 17:48:43 -0700

    Working on writing to json file.

--- Murach/Chapter21.CustomerTxtAndBinary/Chapter21.CustomerTxtAndBinary/Components/JsonWriter.cs
+++ Murach/Chapter21.CustomerTxtAndBinary/Chapter21.CustomerTxtAndBinary/Components/JsonWriter.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.IO;
+using Chapter21.CustomerTxtAndBinary.Models;
+using Newtonsoft.Json;
 
 namespace Chapter21.CustomerTxtAndBinary.Components
 {
@@ -8,9 +11,42 @@ namespace Chapter21.CustomerTxtAndBinary.Components
     {
         public string FilterString => "JSON File|*.json";
 
-        public void WriteTable(FileStream filestream, IEnumerable<DataRow> tableRowCollection)
+        public void WriteTable(FileStream filestream, IEnumerable<DataRow> collection)
         {
-            throw new System.NotImplementedException();
+            using (var streamWriter = new System.IO.StreamWriter(filestream))
+            {
+                using (var jsonWriter = new JsonTextWriter(streamWriter))
+                {
+                    jsonWriter.Formatting = Formatting.Indented;
+                    jsonWriter.IndentChar = '\t';
+
+                    foreach (var dataRow in collection)
+                    {
+                        var items = dataRow.ItemArray;
+                        var customer = Customer.FromItemArray(items);
+
+                        jsonWriter.WriteStartObject();
+                        jsonWriter.WritePropertyName("CustomerID");
+                        jsonWriter.WriteValue(customer.CustomerID);
+                        jsonWriter.WritePropertyName("Name");
+                        jsonWriter.WriteValue(customer.Name);
+                        jsonWriter.WritePropertyName("Address");
+                        jsonWriter.WriteValue(customer.Address);
+                        jsonWriter.WritePropertyName("City");
+                        jsonWriter.WriteValue(customer.City);
+                        jsonWriter.WritePropertyName("State");
+                        jsonWriter.WriteValue(customer.State);
+                        jsonWriter.WritePropertyName("ZipCode");
+                        jsonWriter.WriteValue(customer.ZipCode);
+                        jsonWriter.WriteEndObject();
+
+                        streamWriter.WriteLine();
+                    }
+
+                    jsonWriter.Flush();
+                }
+                    
+            }
         }
     }
 }
