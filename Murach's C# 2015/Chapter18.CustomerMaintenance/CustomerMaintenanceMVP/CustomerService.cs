370032a963f961ff2e6f0026ca182e6d9751f8ae
Rafael Rodriguez <rrodrigu@gmail.com>
2020-01-19 12:36:29 -0700

    Working on new MVP pattern

---
+++ Murach's C# 2015/Chapter18.CustomerMaintenance/CustomerMaintenanceMVP/CustomerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerMaintenanceMVP.DSCustomersTableAdapters;
+
+namespace CustomerMaintenanceMVP
+{
+    class CustomerService
+    {
+        private DSCustomers _customerDataSet;
+
+        public CustomerService(DSCustomers customerDataSet)
+        {
+            _customerDataSet = customerDataSet;
+        }
+
+        public void Load()
+        {
+            var customersTableAdapter = new CustomersTableAdapter();
+
+            customersTableAdapter.Fill(_customerDataSet.Customers);
+        }
+
+        public void Save()
+        {
+            var customersTableAdapter = new CustomersTableAdapter();
+
+            customersTableAdapter.Update(_customerDataSet.Customers);
+        }
+    }
+}
+
