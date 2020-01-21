1889011acd69c0949e636249a6bd9f6f89b204a0
Rafael-Rodriguez <rrodrigu@gmail.com>
2020-01-20 21:50:25 -0700

    Added Customer model

---
+++ Murach's C# 2015/Chapter18.CustomerMaintenance/CustomerMaintenanceMVP/Models/ICustomer.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomerMaintenanceMVP.Models
+{
+    public interface ICustomer
+    {
+        string Name { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string ZipCode { get; set; }
+        int CustomerId { get; set; }
+    }
+}
